b95aad692a7e20603b8e6df53e27b5fbab3a831f
Kishore022000 <kishorejs.srinivasan@gmail.com>
2024-03-18 14:27:39 +0530

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,186 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id:4,
+    emoji:"🤬",
+    name :"Face With Symbols Over The Mouth",
+    meaning:"The symbols over the mouth represent cursing. The serious-looking smiley is really upset and keeps using four-letter words. Represents a sudden outburst of fury or frustration."
+  },
+  {
+    id:5,
+    emoji:"🤭",
+    name :"Face With Hand Over The Mouth",
+    meaning:"Expresses shock, surprise, or a sudden perception. Typical gesture, which shows that the other person is not telling the truth. Thinks he's being lied to."
+  },
+  {
+    id:6,
+    emoji:"🥰",
+    name :"Smiling face with smiling eyes and three hearts",
+    meaning:"Deep connection and loving feelings can be conveyed to the person you are talking to through this emoji. "
+  },
+  {
+    id:7,
+    emoji:"😭",
+    name:"Loudly Crying Face",
+    meaning:"A face crying violently 😭 symbolizes despair and inconsolability, while the tears flow from the eyes like a waterfall. It represents feelings of sadness, pain, or defeat, and is sometimes used in ironic contexts."
+  },
+{
+  id:8,
+  emoji:"😠",
+  name:"Angry Face",
+  meaning:"Smiley is upset, angry, and totally furious. Reaction to something unfavorable or a sign of rejection. You had better keep out of the person's way."
+},
+{
+  id:9,
+  emoji:"🥵",
+  name:"Overheated face",
+  meaning:"It is so hot, I will get a heat stroke right away! Bright red head, the tongue is hanging from the mouth and drops of sweat are on the forehead. The hottest place in the world lies in Iran: In 2005, the highest temperature of 70.7 °C was measured in the Dasht-e Lut desert."
+},
+{
+  id:10,
+  emoji:"🥶",
+  name:"Freezing face",
+  meaning:"It is so cold, you are about to freeze to death. The smile is almost frozen. Already totally blue in the face from the cold temperature, tortured facial expression. Only a cup of tea, coffee, or the hot tub can help now."
+},
+{
+  id:11,
+  emoji:"😱",
+  name:"Face Screaming in Fear",
+  meaning:"Horror-stricken face with two hands, screaming. Is scared stiff, feeling nothing but fear and panic. The creepy emoji can also be used ironically or as a symbol for Halloween."
+},
+{
+  id:12,
+  emoji:"🤔",
+  name:"Thinking Face",
+  meaning:"The thinker's pose: Thoughtful face with the characteristic hand on the chin. Puts a statement, a person's intelligence, or an idea into question. Is mulling over something or thinking about a brilliant idea."
+},
+{
+  id:13,
+  emoji:"😶",
+  name:"Face Without Mouth",
+  meaning:"Emoji without a mouth. Is speechless, has no words. Someone does not want to or cannot say anything about a specific topic. Used in difficult, embarrassing or bad conversations. Imagine what it would say if it had a mouth!"
+},
+{
+  id:14,
+  emoji:"😮",
+  name:"Face With Open Mouth",
+  meaning:"Wow, I'm impressed! Perplexed smiley is looking completely puzzled. Is positively or negatively surprised: from mildly astonished to completely steamrolled."
+},
+{
+  id:15,
+  emoji:"🥱",
+  name:"Yawning face",
+  meaning:"I am dog-tired! The eyes are closed and the hand covers a yawn. The night was too short, you couldn't get enough sleep. Indication that a topic or a person is not of interest, but rather boring."
+},
+{
+  id:16,
+  emoji:"😴",
+  name:"Sleeping Face",
+  meaning:"Good night and sweet dreams! In comics or cartoons, zZz above the head stands for sleeping characters. The emoji is tired, wants to go to bed or is just about to fall asleep from boredom."
+},
+{
+  id:17,
+  emoji:"😵",
+  name:"Dizzy Face",
+  meaning:"I feel dizzy! Smiley with crossed eyes and open mouth is totally dazed. Is confused and so dizzy it doesn't know which side is up. Also symbolizes strong emotions or drunkenness."
+},
+{
+  id:18,
+  emoji:"🤐",
+  name:"Zipper-Mouth Face",
+  meaning:"Smiley with zipper instead of a mouth is keeping a secret for itself. Lips are closed or chat partner's lips should remain closed. Can also mean silence because you cannot find the right words."
+},
+{
+  id:19,
+  emoji:"🤧",
+  name:"Sneezing Face",
+  meaning:"A sneezing face with eyes closed, blowing its nose into a handkerchief. I caught a cold! Shows that someone is ill or feels uncomfortable."
+},
+{
+  id:20,
+  emoji:"🤒",
+  name:"Face With Thermometer",
+  meaning:"The sad face with the thermometer in the mouth expresses illness. Caught a flu or other illness that is associated with fever. Or is worried about getting sick."
+},
+{
+  id:21,
+  emoji:"🤲",
+  name:"Holding hands up, palms against each other",
+  meaning:"I'm praying that everything will go smoothly today or “Can you lend me some money please?“ The upturned palms represent a form of praying or asking for handouts. In American sign language, the symbol of an open book."
+},
+{
+  id:22,
+  emoji:"🤞",
+  name:"Hand With Crossed Fingers",
+  meaning:"The closed fist with crossed index and middle fingers is used to wish someone luck. Children usually use this gesture to annihilate a promise or oath."
+},
+{
+  id:23,
+  emoji:"🤙",
+  name:"Call Me” Hand Sign",
+  meaning:"The closed fist with abducted thumb and little finger has a phone-like shape. “Call me“ or “Let's have a call soon“. Similar to the surfer greeting “Hang loose“"
+},
+{
+  id:24,
+  emoji:"🙏",
+  name:"Person With Folded Hands",
+  meaning:"Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+},
+{
+  id:25,
+  emoji:"😎",
+  name:"Face With Sunglasses",
+  meaning:"I'm the king of the world! Smart and casual, this 😎 emoji features a face with a wide, closed smile and black sunglasses. It expresses coolness and confidence and is used to convey a carefree, relaxed attitude or to express that something is excellent."
+},
+{
+  id:26,
+  emoji:"😏",
+  name:"Smirking/Whimsical Face",
+  meaning:"With a sideways glance, raised eyebrows and a half-smile, the 😏 emoji expresses smugness, irony, coolness or playfulness. It is often used to convey flirtation or sexual innuendo, but use it with caution because sometimes someone might be up to something."
+},
+{
+  id:27,
+  emoji:"😞",
+  name:"Disappointed Face",
+  meaning:"This 😞 emoji shows a face with a sad expression, lowered gaze and down-turned corners of the mouth, reflecting disappointment and frustration. It symbolizes feelings of sadness, regret and remorse, and expresses a range of unhappy emotions that can occur in different situations. "
+},
+{
+  id:28,
+  emoji:"🥺",
+  name:"Face with begging eyes",
+  meaning:"This 🥺 emoji shows a pleading, submissive expression with big, shiny eyes. It is used to ask for something, like money or to borrow a car. Often, the doggy look is the last weapon you need to get what you want when words are not enough. The emoji can also be used to express affection, admiration or emotion through a loving gesture. Forgiveness, compassion and hope are other associations."
+},
+{
+  id:29,
+  emoji:"😋",
+  name:"Face Savouring Delicious Food",
+  meaning:"Bon appetit! Licking your lips with pleasure, the 😋 emoji shows an expression of satisfaction, hunger or joy over delicious food. It is shown beaming with joy when you have cooked something particularly tasty or when your plate is empty."
+},
+{
+  id:30,
+  emoji:"🤝",
+  name:"Handshake",
+  meaning:"Shake on it! The handshake can stand for a greeting or farewell, a sign of agreement or for a deal. Two people are holding hands or, in a figurative sense, to give someone a helping hand."
+}
+];
+export default emojipedia;
